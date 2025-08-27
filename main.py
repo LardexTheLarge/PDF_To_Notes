6867366fbb26372dc21ac7589b3caedea75f681d
LardexTheLarge <0gabevee0@gmail.com>
2025-08-27 14:39:57 -0600

    added code that will save the notes generated by the model to a markdown file.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pdf_reader
 
-chunks = pdf_reader.extract_text_from_pdf("CO_CDL_23.pdf", page_numbers=range(5,6))
+chunks = pdf_reader.extract_text_from_pdf("CO_CDL_23.pdf", page_numbers=range(5,15))
 
 notes = []
 
@@ -8,3 +8,7 @@ for i, chunk in enumerate(chunks):
     print(f"\n\n--- Notes {i+1} ---\n\n")
     note = pdf_reader.summarize_chunk(chunk)
     notes.append(note)
+
+with open("Notes/Intro_notes.md", "w", encoding="utf-8") as file:
+    for i, note in enumerate(notes):
+        file.write(f"\n--- Note {i+1} ---\n{note}")
